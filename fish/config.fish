43f3446d20dd6372605c561074e4e0cc85d28a9b
faizm55212 <faizm55212@gmail.com>
2021-05-21 02:13:52 +0530

    before 20/03

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+alias 'mdrive'='sudo mount /dev/nvme0n1p6 /mnt/drive'
+alias 'udrive'='sudo umount /mnt/drive'
+alias 'ls'='exa -la'
+alias load='kill -USR1 (pidof st)'
+alias use='xrdb merge'
+set -x ANDROID_HOME /home/unknown/Android/Sdk;
+set -x ANDROID_SDK_ROOT /home/unknown/Android/Sdk;
---
+++ polybar/README.md
@@ -0,0 +1,3 @@
+# Packages
+`yay -S nerd-fonts-droid-sans-mono`
+`yay -S otf-font-awesome-5-free`
