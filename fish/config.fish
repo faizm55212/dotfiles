883a0586fb126f2911ac3f3d20e88e9511b21f99
faizm55212 <faizm55212@gmail.com>
2021-06-17 16:06:55 -0400

    kitty config and spaceship

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
-alias 'mdrive'='sudo mount /dev/nvme0n1p6 /mnt/drive'
-alias 'udrive'='sudo umount /mnt/drive'
 alias 'ls'='exa -la'
-alias load='kill -USR1 (pidof st)'
-alias use='xrdb merge'
-set -x ANDROID_HOME /home/unknown/Android/Sdk;
-set -x ANDROID_SDK_ROOT /home/unknown/Android/Sdk;
+set -x STARSHIP_CONFIG ~/.config/fish/config.toml
+
+
+
+starship init fish | source
